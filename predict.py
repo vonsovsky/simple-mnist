c7bd74bee9267f1cfeb8968f229bdddef79994c9
Jakub Vonšovský <jakub.vonsovsky@gmail.com>
2024-12-04 10:29:59 +0100

    Fix prediction

--- predict.py
+++ predict.py
@@ -1,10 +1,13 @@
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import load_model
+import numpy as np
 
 model = load_model('mnist_cnn_model.h5')
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-im1 = x_test.reshape((x_test.shape[1234], 28, 28, 1)).astype('float32') / 255
+im1 = x_test[1234].reshape((1, 28, 28, 1)).astype('float32') / 255
 
 pred = model.predict(im1)
 print(pred)
+pred = np.argmax(pred, axis=1)
+print(pred[0])
